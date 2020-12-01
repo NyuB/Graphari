1a758541c4bb11c28ed9c689a4c648d940786650
NyuB <decaeste@etud.insa-toulouse.fr>
2020-12-01 14:08:04 +0100

    Avoid updated vertex values once fixed

--- src/main/java/insa/sdbd/community/scc/SCCCheckComponentComputation.java
+++ src/main/java/insa/sdbd/community/scc/SCCCheckComponentComputation.java
@@ -10,17 +10,27 @@ import org.apache.giraph.writable.tuple.LongDoubleWritable;
 
 import java.io.IOException;
 
+import org.apache.log4j.Logger;
+
 public class SCCCheckComponentComputation extends BasicComputation<LongWritable,LongWritable, DoubleWritable, LongDoubleWritable> {
+	private static Logger logger = Logger.getLogger(SCCCheckComponentComputation.class);
+
 	@Override
 	public void compute(Vertex<LongWritable, LongWritable, DoubleWritable> vertex, Iterable<LongDoubleWritable> iterable) throws IOException {
+		
 		if(getSuperstep() == 0){
 			vertex.setValue(new LongWritable(SCCMasterComputation.VERTEX_INIT));
 		}
-		
+
 		if((vertex.getValue().get() == SCCMasterComputation.VERTEX_INIT) || (vertex.getValue().get() == SCCMasterComputation.VERTEX_REACHED)) {
-			aggregate(SCCMasterComputation.VERTEX_NOT_CLASSIFIED_AGG,new BooleanWritable(true));
-			aggregate(SCCMasterComputation.VERTEX_UPDATED_AGG, new BooleanWritable(true));
-			aggregate(SCCMasterComputation.CURRENT_VERTEX_AGG, vertex.getId());
+			if(vertex.getNumEdges() == 0){
+				vertex.setValue(vertex.getId());
+			}
+			else{
+				vertex.setValue(new LongWritable(SCCMasterComputation.VERTEX_INIT));
+				aggregate(SCCMasterComputation.VERTEX_UPDATED_AGG, new BooleanWritable(true));
+				aggregate(SCCMasterComputation.CURRENT_VERTEX_AGG, vertex.getId());
+			}
 		}
 		else {
 			vertex.voteToHalt();

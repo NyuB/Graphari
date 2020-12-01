3c3b2eeed6ab42ef7dfd00483b7e3f56adcb723c
NyuB <decaeste@etud.insa-toulouse.fr>
2020-12-01 14:04:24 +0100

    Added labo packaage for giraph functionnalities testing. First file is to test Giraph custom argument API

---
+++ src/main/java/insa/sdbd/community/labo/AssignArgValueComputation.java
@@ -0,0 +1,21 @@
+package insa.sdbd.community.labo;
+
+import org.apache.giraph.conf.LongConfOption;
+import org.apache.giraph.graph.BasicComputation;
+import org.apache.giraph.graph.Vertex;
+import org.apache.hadoop.io.DoubleWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.NullWritable;
+
+import java.io.IOException;
+
+public class AssignArgValueComputation extends BasicComputation<LongWritable, LongWritable, DoubleWritable, NullWritable> {
+	public static LongConfOption ASSIGNED = new LongConfOption("insa.ASSIGNED",42L,"Default value assigned to each vertex");
+	@Override
+	public void compute(Vertex<LongWritable, LongWritable, DoubleWritable> vertex, Iterable<NullWritable> iterable) throws IOException {
+		if(getSuperstep()==0){
+			vertex.setValue(new LongWritable(ASSIGNED.get(getConf())));
+		}
+		vertex.voteToHalt();
+	}
+}
